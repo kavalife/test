daa437650e59a86da92ec58b828356b774ae710d
kavalife <149783601+kavalife@users.noreply.github.com>
2023-11-28 11:25:17 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,16 +3,19 @@
 #include <string>
 #include <Windows.h>
 
+// Клас, що представляє нерухомість
 class RealEstate {
 public:
-    std::string address;
-    std::string type;
-    int price;
+    std::string address;  // Адреса нерухомості
+    std::string type;     // Тип нерухомості
+    double price;         // Вартість нерухомості
 
+    // Конструктор для ініціалізації об'єкту нерухомості
     RealEstate(const std::string& addr, const std::string& t, double p)
         : address(addr), type(t), price(p) {}
 
-    void Display() const {
+    // Метод для відображення інформації про нерухомість
+    void display() const {
         std::cout << "Адреса: " << address << ", Тип нерухомості: " << type << ", Вартість: $" << price << std::endl;
     }
 };
@@ -20,10 +23,12 @@ public:
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    std::vector<RealEstate> realEstates;
+
+    std::vector<RealEstate> realEstates;  // Вектор для зберігання об'єктів нерухомості
     char choice;
 
     do {
+        // Виведення меню
         std::cout << "Твоя агенція нерухомості" << std::endl;
         std::cout << "1. Додати об'єкт" << std::endl;
         std::cout << "2. Показати перелік об'єктів" << std::endl;
@@ -32,34 +37,37 @@ int main() {
         std::cin >> choice;
 
         switch (choice) {
-        case '1':
-        {
-            std::string address, type;
-            double price;
-            std::cin.ignore();
-            std::cout << "Введіть адресу: ";
-            std::getline(std::cin, address);
-            std::cout << "Введіть тип нерухомості: ";
-            std::getline(std::cin, type);
-            std::cout << "Введіть вартість: ";
-            std::cin >> price;
+            case '1': {
+                std::string address, type;
+                double price;
 
-            RealEstate newRealEstate(address, type, price);
-            realEstates.push_back(newRealEstate);
-            std::cout << "Об'єкт додано успішно." << std::endl;
-        }
-        break;
-        case '2':
-            std::cout << "Список внесених об'єктів:" << std::endl;
-            for (const RealEstate& property : realEstates) {
-                property.Display();
+                // Введення даних для нового об'єкту нерухомості
+                std::cin.ignore();
+                std::cout << "Введіть адресу: ";
+                std::getline(std::cin, address);
+                std::cout << "Введіть тип нерухомості: ";
+                std::getline(std::cin, type);
+                std::cout << "Введіть вартість: ";
+                std::cin >> price;
+
+                // Створення та додавання нового об'єкту нерухомості до вектору
+                RealEstate newRealEstate(address, type, price);
+                realEstates.push_back(newRealEstate);
+                std::cout << "Об'єкт додано успішно." << std::endl;
+                break;
             }
-            break;
-        case '3':
-            std::cout << "Бувай!" << std::endl;
-            break;
-        default:
-            std::cout << "Помилковий вибір, спробуйте ще раз" << std::endl;
+            case '2':
+                // Виведення переліку внесених об'єктів нерухомості
+                std::cout << "Список внесених об'єктів:" << std::endl;
+                for (const RealEstate& property : realEstates) {
+                    property.display();
+                }
+                break;
+            case '3':
+                std::cout << "Бувай!" << std::endl;
+                break;
+            default:
+                std::cout << "Помилковий вибір, спробуйте ще раз" << std::endl;
         }
     } while (choice != '0');
 
